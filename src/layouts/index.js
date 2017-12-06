c94c9c71339ec69f689112104ff5046a6197c8c5
Caius Brindescu <brindesc@oregonstate.edu>
2017-12-06 12:08:26 -0800

    A version of the design.
    Added the main page and some of the assests.

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,13 +2,73 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
+import Typography from 'typography'
+import 'font-awesome/css/font-awesome.min.css'
 
 import './index.css'
+
+const typography = new Typography({
+  baseFontSize: "18px",
+  baseLineHeight: 1.45,
+  headerFontFamily: [
+    "Avenir Next",
+    "Helvetica Neue",
+    "Segoe UI",
+    "Helvetica",
+    "Arial",
+    "sans-serif",
+  ],
+  bodyFontFamily: ["Georgia", "serif"],
+});
+
+const ExternalListLink = props =>
+  <li style={{
+    padding: '0 auto',
+    margin: '0 auto',
+    display: 'inline-block',
+    marginRight: `0.5rem`,
+    verticalAlign: 'middle'
+  }}>
+    <a href={props.to}
+      style={{
+        // padding: '0 auto',
+        // margin: '0 auto',
+        color: 'inherit',
+        fontSize: '1.5rem',
+        verticalAlign : 'middle',
+        textDecoration: 'none',
+        float: 'right',
+        height: '62px'
+      }}
+    >
+      {props.children}
+    </a>
+  </li>
+
+const InternalListLink = props =>
+  <li style={{
+    bodyFontFamily: typography.headerFontFamily,
+    display: 'inline-block',
+    margin: '0.5rem 1.0875rem',
+    padding: '0.5rem 0rem',
+    verticalAlign: 'middle'
+   }}>
+    <Link to={props.to}
+      style={{
+        color: 'inherit',
+        fontSize: '1.25rem',
+        verticalAlign : 'middle',
+        textDecoration: 'none',
+        float: 'left'
+      }}
+    >
+      {props.children}
+    </Link>
+  </li>
 
 const Header = () => (
   <div
     style={{
-      background: 'rebeccapurple',
       marginBottom: '1.45rem',
     }}
   >
@@ -19,17 +79,59 @@ const Header = () => (
         padding: '1.45rem 1.0875rem',
       }}
     >
-      <h1 style={{ margin: 0 }}>
+      <h1 style={{ margin: 0, display: 'inline' }}>
         <Link
           to="/"
           style={{
-            color: 'white',
+            color: 'inherit',
             textDecoration: 'none',
           }}
         >
-          Gatsby
+          Caius Brindescu
         </Link>
       </h1>
+    </div>
+    <div style={{
+      maxWidth: '960px',
+      verticalAlign: 'middle',
+      margin: '0 auto'
+      // lineHeight: '1.25rem'
+    }}>
+      <ul style={{
+        margin: '0 auto',
+        padding: '0 auto',
+        verticalAlign: 'middle',
+        display: 'inline-block'
+      }}>
+        <InternalListLink to="research.html">Research</InternalListLink>
+        <InternalListLink to="projects.html">Projects</InternalListLink>
+        <InternalListLink to="cycling.html">Cycling</InternalListLink>
+        <InternalListLink to="blog.html">Blog</InternalListLink>
+      </ul>
+      <ul style={{
+        margin: '0 auto',
+        padding: '0 auto',
+        listStyle: 'none',
+        float: 'right',
+        align: 'center',
+        baseLineHeight: 'inherit',
+        verticalAlign: 'middle',
+        dislay: 'inline-block',
+        height: '62px'
+      }}>
+        <ExternalListLink to="https://www.github.com/caiusb">
+          <i className="fa fa-github-square"></i>
+        </ExternalListLink>
+        <ExternalListLink to="https://www.twitter.com/caiusbrindescu">
+          <i className="fa fa-twitter-square"></i>
+        </ExternalListLink>
+        <ExternalListLink to="https://www.linkedin.com/in/caius-brindescu-b9060b153/">
+          <i className="fa fa-linkedin-square"></i>
+        </ExternalListLink>
+        <ExternalListLink to="mailto:caius@brindescu.com">
+          <i className="fa fa-envelope-square"></i>
+        </ExternalListLink>
+      </ul>
     </div>
   </div>
 )
@@ -37,10 +139,10 @@ const Header = () => (
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
-      title="Gatsby Default Starter"
+      title="Caius Brindescu"
       meta={[
         { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: 'keywords', content: 'caius, brindescu, oregon, state, university, computer, science, software, engineering' },
       ]}
     />
     <Header />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,28 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import styles from './index.module.css'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>About me</h1>
+    <div>
+      <img className={styles.avatar} src="/pics/me_niagara_falls.jpeg" />
+
+      <p>
+      I am a PhD Student at <a href="http://eecs.oregonstate.edu">Oregon State University</a>.
+      I am originaly from <a href="http://en.wikipedia.org/wiki/Timisoara">Timisoara, Romania</a>.
+      I did my BS in Computer and Information Technology at the "Politehnica" University of Timisoara.
+      My advisor was <a href="http://bigfoot.cs.upt.ro/~radum/">Radu Marinescu</a>.
+      My area of interest is Software Engineering with an accent on Software Evolution and Version Control.
+      </p>
+      <p>
+      I am part of the <a href="http://eecs.oregonstate.edu/node/268">Software Engineering, Usability, and Programming Languages group</a>.
+      I work with <a href="http://eecs.oregonstate.edu/people/jensen-carlos">Carlos Jensen</a>.
+      </p>
+      <p>
+      My CV is available <a href="CV-CaiusBrindescu.pdf">here</a>.
+      </p>
+    </div>
   </div>
 )
 
