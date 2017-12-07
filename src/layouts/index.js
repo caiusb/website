5fc40b58cd78b28d885c4e73390eb3d3480ad37f
Caius Brindescu <brindesc@oregonstate.edu>
2017-12-06 17:06:24 -0800

    Moved the css out of the main file

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -6,6 +6,7 @@ import Typography from 'typography'
 import 'font-awesome/css/font-awesome.min.css'
 
 import './index.css'
+import styles from './index.module.css'
 
 const typography = new Typography({
   baseFontSize: "18px",
@@ -22,87 +23,34 @@ const typography = new Typography({
 });
 
 const ExternalListLink = props =>
-  <li style={{
-    display: 'inline-block',
-    marginRight: '6pt',
-    marginBottom: '0pt'
-  }}>
-    <a href={props.to}
-      style={{
-        color: 'inherit',
-        fontSize: '22pt',
-        textDecoration: 'none',
-      }}
-    >
+  <li className={styles.socialIcons}>
+    <a href={props.to}>
       {props.children}
     </a>
   </li>
 
 const InternalListLink = props =>
-  <li style={{
-    bodyFontFamily: typography.headerFontFamily,
-    display: 'inline-block',
-    margin: '0.5rem 1.0875rem',
-    padding: '0.5rem 0rem',
-    verticalAlign: 'middle'
-   }}>
-    <Link to={props.to}
-      style={{
-        color: 'inherit',
-        fontSize: '16pt',
-        verticalAlign : 'middle',
-        textDecoration: 'none',
-        float: 'left'
-      }}
-    >
+  <li className={styles.internalHeaderLink}>
+    <Link to={props.to}>
       {props.children}
     </Link>
   </li>
 
 const Header = () => (
-  <div
-    style={{
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        paddingLeft: '1.0875rem',
-        paddingTop: '1.45rem',
-        paddingBotton: '0.5rem'
-      }}
-    >
-      <h1 style={{ margin: 0, display: 'inline' }}>
-        <Link
-          to="/"
-          style={{
-            color: 'inherit',
-            textDecoration: 'none',
-          }}
-        >
-          Caius Brindescu
-        </Link>
+  <div style={{marginBottom: '1.45rem'}}>
+    <div className={styles.header}>
+      <h1>
+        <Link to="/">Caius Brindescu</Link>
       </h1>
-      <ul style={{
-        // margin: '0 auto',
-        // padding: '0 auto',
-        verticalAlign: 'middle',
-        display: 'inline-block',
-        float: 'right'
-      }}>
+      <ul className={styles.internalListLink}>
         <InternalListLink to="research.html">Research</InternalListLink>
         <InternalListLink to="projects.html">Projects</InternalListLink>
         <InternalListLink to="cycling.html">Cycling</InternalListLink>
         <InternalListLink to="blog.html">Blog</InternalListLink>
       </ul>
     </div>
-    <div style={{maxWidth: '960px', marginLeft: 'auto', marginRight: 'auto'}}>
-      <ul style={{
-        listStyle: 'none',
-        align: 'center'
-      }}>
+    <div className={styles.socialIconsList}>
+      <ul className={styles.externalListLink}>
         <ExternalListLink to="https://www.github.com/caiusb">
           <i className="fa fa-github-square"></i>
         </ExternalListLink>
@@ -117,12 +65,6 @@ const Header = () => (
         </ExternalListLink>
       </ul>
     </div>
-    <div style={{
-      maxWidth: '960px',
-      verticalAlign: 'middle',
-      margin: '0 auto'
-    }}>
-    </div>
   </div>
 )
 
@@ -136,14 +78,7 @@ const TemplateWrapper = ({ children }) => (
       ]}
     />
     <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
+    <div className={styles.container}>
       {children()}
     </div>
   </div>
