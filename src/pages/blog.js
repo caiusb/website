21e561b444c8dc617e68ba57373ce2ebe8f96ad6
Caius Brindescu <brindesc@oregonstate.edu>
2018-01-03 13:13:10 -0800

    Added stubs for the rest of the pages

---
+++ src/pages/blog.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import common from './common.module.css'
+
+const BlogPage = () => (
+  <div>
+    <h1>Blog</h1>
+
+    <h2>Under Contruction</h2>
+  </div>
+)
+
+export default BlogPage
---
+++ src/pages/cycling.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import common from './common.module.css'
+
+const CyclingPage = () => (
+  <div>
+    <h1>Cycling</h1>
+
+    <h2>Under Contruction</h2>
+  </div>
+)
+
+export default CyclingPage
