2583e21510018ff1fd63062eb14baa917d069c07
Caius Brindescu <caius.brindescu@gmail.com>
2018-06-09 13:37:21 -0700

    Updated the main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,15 +9,15 @@ const IndexPage = () => (
       <img className={styles.avatar} src="/pics/me_niagara_falls.jpeg" />
 
       <p>
-      I am a PhD Student at <a href="http://eecs.oregonstate.edu">Oregon State University</a>.
+      I am a PhD Candidate at <a href="http://eecs.oregonstate.edu">Oregon State University</a>.
       I am originaly from <a href="http://en.wikipedia.org/wiki/Timisoara">Timisoara, Romania</a>.
-      I did my BS in Computer and Information Technology at the "Politehnica" University of Timisoara.
+      I did my B.Sc. in Computer and Information Technology at the "Politehnica" University of Timisoara.
       My advisor was <a href="http://bigfoot.cs.upt.ro/~radum/">Radu Marinescu</a>.
       My area of interest is Software Engineering with an accent on Software Evolution and Version Control.
       </p>
       <p>
       I am part of the <a href="http://eecs.oregonstate.edu/node/268">Software Engineering, Usability, and Programming Languages group</a>.
-      I work with <a href="http://eecs.oregonstate.edu/people/jensen-carlos">Carlos Jensen</a>.
+      I work with <a href="http://eecs.oregonstate.edu/people/jensen-carlos">Carlos Jensen</a> and <a href="http://eecs.oregonstate.edu/people/sarma-anita">Anita Sarma</a>
       </p>
       <p>
       My CV is available <a href="CV-CaiusBrindescu.pdf">here</a>.
