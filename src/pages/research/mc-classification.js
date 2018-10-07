e8d326082b995cf6f2490128568f1061f6db74d1
Caius Brindescu <caius.brindescu@gmail.com>
2018-10-07 14:11:29 -0400

    Added companion website

---
+++ src/pages/research/mc-classification.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import styles from '../index.module.css'
+
+const McClassification = () => (
+    <div>
+        <h1>Under Construction!</h1>
+    </div>
+);
+
+export default McClassification;
