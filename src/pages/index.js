f11a63d275561228aa5fe061b099ea28d00d0634
Caius Brindescu <caius@brindescu.com>
2020-10-29 17:04:29 -0700

    Updated main and research page

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,17 +9,19 @@ const IndexPage = () => (
       <img className={styles.avatar} src="/pics/me_niagara_falls.jpeg" />
 
       <p>
-      I am a software enginneer at <a href="https:/www.etleap.com">Etleap</a> and a Ph.D. Candidate at <a href="http://eecs.oregonstate.edu">Oregon State University</a>.
+      I am a software enginneer at <a href="https:/www.etleap.com">Etleap</a>. I got my Ph.D. Degree at <a href="http://eecs.oregonstate.edu">Oregon State University,</a> under the mentorship for Dr. Anita Sarma and Dr. Carlos Jensen.
       Originally from <a href="http://en.wikipedia.org/wiki/Timisoara">Timisoara, Romania</a>, I did my B.Sc. in Computer and Information Technology at the <a href="https://www.upt.ro">"Politehnica" University of Timisoara</a>, under the guidance of <a href="http://bigfoot.cs.upt.ro/~radum/">Radu Marinescu</a>.
-      My research interests include Software Engineering, Refactorings, Program Transformations, Program Analysis and Developer Processes.
-      I aim to make developers' lives easier by building tools that better suit their needs.
+      My research focused on understanding where tools fail developers, with a focus on Merge Conflict Resolution.
+      More details can be found in my <a href="CaiusBrindescuPhDThesis.pdf">thesis,</a> and under on my <a href="/research">research page.</a>
       </p>
       <p>
-      I am part of the <a href="http://eecs.oregonstate.edu/node/268">Software Engineering, Usability, and Programming Languages group</a>.
-      I am currently advised by <a href="http://eecs.oregonstate.edu/people/jensen-carlos">Carlos Jensen</a> and <a href="http://eecs.oregonstate.edu/people/sarma-anita">Anita Sarma</a>.
+      I have over 15 years experience using Java, and related technologies.
+      My current work focuses developing and maintaing Java a backend, and working with Hadoop, Spark, and managed AWS services.
+      I have experience in maintaing infrastructure as code with both Terraform and CloudFormation.
+      Finally, I also have extensive experience in Scala, Python, Go, C, Bash, TypeScript and JavaScript.
       </p>
       <p>
-      My CV is available <a href="CV-CaiusBrindescu.pdf">here</a>.
+      My full CV is available <a href="CV-CaiusBrindescu.pdf">here</a>.
       </p>
     </div>
   </div>
--- src/pages/research.js
+++ src/pages/research.js
@@ -8,20 +8,45 @@ const ProjectsPage = () => (
 
     <h2>Research Statement</h2>
     <p>
-    Currently, source code is treated like plain text. I think this is wrong.
+    Currently, source code is treated like plain text. 
+    I think this is wrong.
     Treating it like text poses great problems when it comes to understanding it.
     A developer first needs to understand the code, before they can start making changes.
     My goal is to change this.
     </p>
     <p>
-    My current work is on improving parallel software development.
-    I am looking at merging practices with the hopes of reducing conflicts and
-    improving their resolution. I am also interested in software testing,
-    especially mutation testing.
+    My work focused on improving parallel software development.
+    By looking at the merging practices, I made strides in understanding why merge conflict are a problem from both a code perpective, and from the developers' perpective. 
+    I also conducted research in software testing, especially mutation testing.
     </p>
 
     <h2>Publications</h2>
     <ul>
+      <li>
+        <i>Using Relative Lines of Code to Guide Automated Test Generation for Python</i><br />
+        Josie Holmes, Iftekhar Ahmed, <i>Caius Brindescu</i>, Rahul Gopinath, He Zhang, Alex Groce<br />
+        ACM Transactions on Software Engineering (TOSEM), Volume 29, Issue 4.
+      </li>
+      <li>
+        <i>Lifting the Curtain on Merge Conflict Resolution: A Sensemaking Perspective</i><br />
+        <i>Caius Brindescu</i>, Yenifer Ramirez, Anita Sarma, Carlos Jensen<br />
+        ICSME 2020: 36th IEEE International Conference on Software Maintenance and Evolution
+      </li>
+      <li>
+        <i>Planning for untangling: Predicting the Difficulty of Merge Conflicts</i><br />
+        <i>Caius Brindescu</i>, Iftekhar Ahmed, Rafael Leano, Anita Sarma<br />
+        ICSE 2020, The 42nd International Conference on Software Engineering 
+      </li>
+      <li>
+        <i>An Empirical Investigation into Merge Conflicts and Their Effect on Software Quality</i><br />
+        <i>Caius Brindescu</i>, Iftekhar Ahmed, Carlos Jensen, Anita Sarma<br />
+        Empirical Software Engineering, Volume 25, Issue 1
+      </li>
+      <li>
+        <i>The Life-Cycle of Merge Conflicts: Processes, Barriers, and Strategies</i><br />
+        Nicholas Nelson, <i>Caius Brindescu</i>, Shane McKee, Anita Sarma, Danny Dig <br />
+        Empirical Software Engineering, Volume 24, Issue 4.
+      </li>
       <li>
         <i>An Empirical Examination of the Relationship Between Code Smells and Merge Conflicts</i><br />
 
