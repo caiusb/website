58d1d217d0294f895d5bbb267155ec61160bf644
Caius Brindescu <caius@brindescu.com>
2024-01-05 01:49:36 +0000

    Updated links

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,8 +10,8 @@ const IndexPage = () => (
       <img class={styles.avatar} src="/pics/me_niagara_falls.jpeg" alt="Headshot"/>
 
       <p>
-      I am a software enginneer at <a href="https:/www.etleap.com">Etleap</a>. I got my Ph.D. Degree at <a href="http://eecs.oregonstate.edu">Oregon State University,</a> under the mentorship for Dr. Anita Sarma and Dr. Carlos Jensen.
-      Originally from <a href="http://en.wikipedia.org/wiki/Timisoara">Timisoara, Romania</a>, I did my B.Sc. in Computer and Information Technology at the <a href="https://www.upt.ro">"Politehnica" University of Timisoara</a>, under the guidance of <a href="http://bigfoot.cs.upt.ro/~radum/">Radu Marinescu</a>.
+      I am a software enginneer at <a href="https:/www.etleap.com">Etleap</a>. I got my Ph.D. Degree at <a href="http://eecs.oregonstate.edu">Oregon State University,</a> under the mentorship for <a href="https://web.engr.oregonstate.edu/~sarmaa/">Dr. Anita Sarma</a> and <a href="https://educationalinnovation.ucsd.edu/about/jensen-bio.html">Dr. Carlos Jensen.</a>
+      Originally from <a href="http://en.wikipedia.org/wiki/Timisoara">Timisoara, Romania</a>, I did my B.Sc. in Computer and Information Technology at the <a href="https://www.upt.ro">"Politehnica" University of Timisoara</a>, under the guidance of <a href="https://staff.cs.upt.ro/~radum/">Radu Marinescu</a>.
       My research focused on understanding where tools fail developers, with a focus on Merge Conflict Resolution.
       More details can be found in my <a href="CaiusBrindescuPhDThesis.pdf">thesis,</a> and under on my <Link to="/research">research page.</Link>
       </p>
@@ -22,7 +22,7 @@ const IndexPage = () => (
       Finally, I also have extensive experience in Scala, Python, Go, C, Bash, TypeScript and JavaScript.
       </p>
       <p>
-      My full CV is available <a href="CV-CaiusBrindescu.pdf">here</a>.
+      My full CV is available <a href="CV-CaiusBrindescu.pdf">here</a>, and my resume <a href="https://caius.notion.site/Caius-Brindescu-PhD-108fc0091f70495d8659ffbef25b8c4c?pvs=4">here.</a>
       </p>
     </div>
   </Layout>
