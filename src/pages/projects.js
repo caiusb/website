2dcc7684e3fec5d67aabf44ac1296ac53b59fa81
Caius Brindescu <caius@brindescu.com>
2024-12-09 22:07:16 -0800

    Revert "Fixed GitHub icons on project's page"
    This reverts commit a47f9040d526e5f25a68938d423000ed46496566.

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Layout from '../components/layout'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const ProjectsPage = () => (
   <Layout>
@@ -10,13 +9,13 @@ const ProjectsPage = () => (
 
     <ul>
       <li>
-        <h3><a href="https://github.com/caiusb/website">This website <FontAwesomeIcon icon={["fa-brands", "fa-square-github"]} /></a></h3>
+        <h3><a href="https://github.com/caiusb/website">This website <i className='fa fa-github-square'></i></a></h3>
         <p>
           A simple Gatsby website, nothing fancy.
         </p>
       </li>
       <li>
-        <h3><a href="https://github.com/caiusb/JDTFacade">JDT Facade <FontAwesomeIcon icon={["fa-brands", "fa-square-github"]} /></a></h3>
+        <h3><a href="https://github.com/caiusb/JDTFacade">JDT Facade <i className='fa fa-github-square'></i></a></h3>
         <p>
           A Scala wrap of the Eclipse JDT compiler, without Eclipse. An easy way
           to parse Java files, and get the corresponding AST. It also enhances
@@ -24,7 +23,7 @@ const ProjectsPage = () => (
         </p>
       </li>
       <li>
-        <h3><a href="https://github.com/caiusb/gumtree-facade">GumTree facade <FontAwesomeIcon icon={["fa-brands", "fa-square-github"]} /></a></h3>
+        <h3><a href="https://github.com/caiusb/gumtree-facade">GumTree facade <i className='fa fa-github-square'></i></a></h3>
         <p>
           A Scala Facade build around the <a href="https://github.com/GumTreeDiff/gumtree">GumTree</a> diff
           tool. It makes it easy to get and AST difference between to Java
