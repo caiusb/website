d02a41b1407c8bd06b1eaa94f31832294272d79f
Caius Brindescu <caius.brindescu@gmail.com>
2025-06-07 13:56:01 -0700

    Fixing header icons not showing when going directly to a webpage (#102)
    Fixes #93

---
+++ src/components/fontawesome.js
@@ -0,0 +1,16 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { 
+  faSquareGithub,
+  faSquareBluesky,
+  faSquareThreads,
+  faLinkedin,
+} from '@fortawesome/free-brands-svg-icons'
+import { faSquareEnvelope } from '@fortawesome/free-solid-svg-icons'
+
+library.add(
+  faSquareGithub,
+  faSquareBluesky,
+  faSquareThreads,
+  faLinkedin,
+  faSquareEnvelope
+) 
