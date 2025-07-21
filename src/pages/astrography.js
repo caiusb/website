88191634bebcfc37b33e0f5fecb14d793865435d
Caius Brindescu <caius.brindescu@gmail.com>
2025-07-21 03:26:47 +0000

    Added text and image to page

--- src/pages/astrography.js
+++ src/pages/astrography.js
@@ -11,6 +11,9 @@ export default function Astrography() {
             <h3>M17 - Omega Nebula</h3>
             <p>Images were taken on June 29th, 2025, processed with Siril with a total integration time of 10 minutes.</p>
             <img src="/astrography/M17-20250629-10mins-IRCUT.png" width="500" alt="M17"/>
+            <h2>M20 - Trifid Nebula</h2>
+            <p>Images were taken on June 30th, 2025, processed with Siril with a total integration time of 32 minutes.</p>
+            <img src="/astrography/M20-20250630-32mins-IRCUT.png" width="500" alt="M20"/>
         </Layout>
     );
 }
