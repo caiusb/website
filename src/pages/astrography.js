edea87e18cdc5df75860434254704741981336d6
Caius Brindescu <caius@brindescu.com>
2026-01-23 19:21:51 +0000

    Converted astro images to webp lossless

--- src/pages/astrography.js
+++ src/pages/astrography.js
@@ -10,10 +10,10 @@ export default function Astrography() {
             <h2>Select images</h2>
             <h3>M17 - Omega Nebula</h3>
             <p>Images were taken on June 29th, 2025, processed with Siril with a total integration time of 10 minutes.</p>
-            <img src="/astrography/M17-20250629-10mins-IRCUT.png" width="500" alt="M17"/>
+            <img src="/astrography/M17-20250629-10mins-IRCUT.webp" width="500" alt="M17"/>
             <h2>M20 - Trifid Nebula</h2>
             <p>Images were taken on June 30th, 2025, processed with Siril with a total integration time of 32 minutes.</p>
-            <img src="/astrography/M20-20250630-32mins-IRCUT.png" width="500" alt="M20"/>
+            <img src="/astrography/M20-20250630-32mins-IRCUT.webp" width="500" alt="M20"/>
         </Layout>
     );
 }
